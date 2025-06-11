5d1f08939f7f63b10f694274832bd2ce3b067cc1
ivanlele <ivanlele402@gmail.com>
2025-06-11 14:24:09 +0300

    Update ZkNeuralRustCoreFramework and TensorFlowLiteC URLs and checksums; remove unused test assets

--- Package.swift
+++ Package.swift
@@ -22,13 +22,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ZkNeuralRustCoreFramework",
-            url: "https://github.com/rarimo/zk-neural-rust-core/releases/download/v0.1.5/ZkNeuralRustCore.xcframework.zip",
+            url: "https://github.com/rarimo/zk-neural-rust-core/releases/download/v0.1.6/ZkNeuralRustCore.xcframework.zip",
             checksum: "01bbf04a9787a1023668fa43cdfc57d048394e9e14be37a0182e0b570cac0b42"
         ),
         .binaryTarget(
             name: "TensorFlowLiteC",
-            url: "https://github.com/rarimo/zk-neural-rust-core/releases/download/v0.1.5/TensorFlowLiteC.xcframework.zip",
-            checksum: "e77c317adc7c3a65179c14abcc93322150f9ccb972a3b0a1b52b5ff91ab99815"
+            url: "https://github.com/rarimo/zk-neural-rust-core/releases/download/v0.1.6/TensorFlowLiteC.xcframework.zip",
+            checksum: "18a039945175352e168da9c3ff2ab202b9b56484460b5526925f7a72f8a76e8a"
         ),
         .testTarget(
             name: "ZKNeuralSwiftCoreTests",
--- Tests/ZKNeuralSwiftCoreTests/ZKNeuralSwiftCoreTests.swift
+++ Tests/ZKNeuralSwiftCoreTests/ZKNeuralSwiftCoreTests.swift
@@ -1,12 +1,4 @@
-import Foundation
-
 import Testing
 @testable import ZKNeuralSwiftCore
 
-@Test func testTensorInvoker() async throws {
-    let model_path = Bundle.main.path(forResource: "BioNetV3", ofType: "tflite")
-
-    print("Model path: \(model_path ?? "Not found")")
-
-//    let invoker = TensorInvoker()
-}
+@Test func testTensorInvoker() async throws {}
