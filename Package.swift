57868aacbd7870dcc25a55aa486cbefda41a218a
ivanlele <ivanlele402@gmail.com>
2025-06-11 13:50:18 +0300

    Add TensorFlowLiteC dependency and test assets for TensorInvoker

--- Package.swift
+++ Package.swift
@@ -17,13 +17,18 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ZKNeuralSwiftCore",
-            dependencies: ["ZkNeuralRustCoreFramework"],
+            dependencies: ["ZkNeuralRustCoreFramework", "TensorFlowLiteC"],
             linkerSettings: [.linkedFramework("SystemConfiguration")]
         ),
         .binaryTarget(
             name: "ZkNeuralRustCoreFramework",
             url: "https://github.com/rarimo/zk-neural-rust-core/releases/download/v0.1.5/ZkNeuralRustCore.xcframework.zip",
             checksum: "01bbf04a9787a1023668fa43cdfc57d048394e9e14be37a0182e0b570cac0b42"
+        ),
+        .binaryTarget(
+            name: "TensorFlowLiteC",
+            url: "https://github.com/rarimo/zk-neural-rust-core/releases/download/v0.1.5/TensorFlowLiteC.xcframework.zip",
+            checksum: "e77c317adc7c3a65179c14abcc93322150f9ccb972a3b0a1b52b5ff91ab99815"
         ),
         .testTarget(
             name: "ZKNeuralSwiftCoreTests",
--- Tests/ZKNeuralSwiftCoreTests/ZKNeuralSwiftCoreTests.swift
+++ Tests/ZKNeuralSwiftCoreTests/ZKNeuralSwiftCoreTests.swift
@@ -1,6 +1,12 @@
+import Foundation
+
 import Testing
 @testable import ZKNeuralSwiftCore
 
-@Test func testCore() async throws {
-//    let core = ZKNeuralCore()
+@Test func testTensorInvoker() async throws {
+    let model_path = Bundle.main.path(forResource: "BioNetV3", ofType: "tflite")
+
+    print("Model path: \(model_path ?? "Not found")")
+
+//    let invoker = TensorInvoker()
 }
